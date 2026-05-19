d08558a916d4d49a4c0acc4fc1e87b50f004973e
jonaslynge <j.lynge000@gmail.com>
2026-05-19 13:42:56 +0200

    Opdateret insert_products og ny database

--- insert_products.py
+++ insert_products.py
@@ -4,11 +4,11 @@ conn = sqlite3.connect("database.db")
 cursor = conn.cursor()
 
 products = [
-    ("27000000000014", "Spegepølse", 34.95, "Databasebillederne/spegepoelse.png", "Pålæg"),
-    ("27000000000021", "Rugbrød", 24.95, "Databasebillederne/rugbroed.png", "Brød"),
-    ("27000000000038", "Rejesalat", 29.95, "Databasebillederne/rejesalat.png", "Pålægssalater"),
-    ("27000000000045", "Pastasalat", 34.95, "Databasebillederne/pastasalat.png", "Færdigretter"),
-    ("2700000000005", "Russisk salat", 24.95, "Databasebillederne/russisk_salat.png", "Pålægssalater")
+    ("270000000014", "Spegepølse", 34.95, "Databasebillederne/spegepoelse.png", "Pålæg"),
+    ("270000000021", "Rugbrød", 24.95, "Databasebillederne/rugbroed.png", "Brød"),
+    ("270000000038", "Rejesalat", 29.95, "Databasebillederne/rejesalat.png", "Pålægssalater"),
+    ("270000000045", "Pastasalat", 34.95, "Databasebillederne/pastasalat.png", "Færdigretter"),
+    ("270000000052", "Russisk salat", 24.95, "Databasebillederne/russisk_salat.png", "Pålægssalater")
 ]
 
 cursor.executemany("""
